37f1839e8892bfa1f001ea0752948e13874de366
Rujul Shah <rujuls@hotmail.com>
2021-11-10 05:32:03 -0800

    Commit 1 on master

---
+++ src/test/java/Base/BaseTest.java
@@ -0,0 +1,23 @@
+package Base;
+import java.util.*;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class BaseTest {
+
+		public static WebDriver driver;
+		public static ExtentReports report;
+		public static ExtentTest test;
+		public static ExtentHtmlReporter htmlreporter;
+		
+		
+		
+
+	
+
+}
